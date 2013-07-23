475da8b22467f9b4680c1810abb53a337e06b83b
supabok <superbok@superbokbok.com>
2013-07-23 18:03:39 +0900

    failing test purposely

--- src/test/random.test.js
+++ src/test/random.test.js
@@ -23,7 +23,7 @@ describe('addition', function () {
     'use strict';
 
     it('should return 4 when adding 2 and 2', function () {
-        expect(2 + 2).toBe(4);
+        expect(2 + 3).toBe(4);
     });
 });
 
